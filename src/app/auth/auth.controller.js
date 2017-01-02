0c2cdf80bcb39acaa59cd9b1e078a5d98448c4f2
Skúli Freyr Hinriksson <skuli@sareye.com>
2017-01-02 22:22:27 +0100

    Implemented the login functionality

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -4,9 +4,17 @@ export class AuthController {
 
         this.$auth = $auth;
     }
+    
     register() {
         var vm = this;
         this.$auth.signup(this.user).then(function(token) {
+            vm.$auth.setToken(token);
+        });
+    }
+    
+    login() {
+        var vm = this;
+        this.$auth.login(this.login.user).then(function(token) {
             vm.$auth.setToken(token);
         });
     }
